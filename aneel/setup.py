7cef7960a74ae202cf036560f98e53ba177ecce9
Renato Sousa <renato.ef.sousa@gmail.com>
2016-08-07 22:00:59 -0300

    arquivo setup
    Criação do arquivo de setup

---
+++ aneel/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="aneel",
+    version="0.0.1",
+    url="https://github.com/mstuttgart/codigo-avulso-test-tutorial",
+    license="MIT License",
+    author="Renato Eduardo Farias de Sousa",
+    author_email="renato.ef.sousa@gmail.com",
+    keywords="aneel cálculos regulamentos",
+    description=u"Esse pacote realiza diversos cálculo relacioandos a regulamentos da ANEEL",
+    packages=["aneel"],
+    install_requires=["csv", "json"],
+)
